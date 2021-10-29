5768aab2a0842c7c208e6ee99ab6f37d0fc9dcf4
Dzhan Rafetov <49691399+DzhanRafetov-UKTC@users.noreply.github.com>
2021-10-29 11:38:45 +0300

    0.3
    dobaven while cikul

--- Store.java
+++ Store.java
@@ -22,25 +22,29 @@ public class Store {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Store first = new Store("F1", "USA");
-  /* TODO  май трябва да има един while цикъл който като се натисне 0 да връща назад  */
-        int choice = scan.nextInt();
-/* TODO  Колите които сме добавили от конзолата да отиват при тези които ги има в програма,които сме ги добавили
-     ръчно и ако същестува същата кола да не може... */
-        //Това тук дали ще е по-добре с switch case?//защото като  има default: ако потребителя сгреши нещо
-        //при въвеждането да го върнем на същото място където е сгрешил
-        if (choice == 1) {//ако се натисне 1  извиква се метода за добавяне на коли
+        int choice = -1;
 
-            Utilities.addCars();
+        System.out.println("What you want to make: \n" +
+                "1. Add Vehicle \n" +
+                "2. Show Accepted Applicants \n" +
+                "0. To exit");
+        while (choice != 0) {
+            choice = scan.nextInt();
+
+            if (choice == 1) {
+                Utilities.addCars();
+
+            }
+            if (choice == 2) {
+                Utilities.newApplicant();
+
+            }
         }
-        //Ако се натисне 2- показва кои са назначени
-/* TODO от конзолата да въвеждаме данните на работодателя ако искаме да добавяме.Но да има и работници,които
-    сме добавили  ръчно.И ако съществува такъв работник да не може да добави.
-  */
-        if(choice == 2){
-           Utilities.newApplicant();
-        }
+        System.out.println("You are exiting");
 
     }
+
+
 }
 
 
